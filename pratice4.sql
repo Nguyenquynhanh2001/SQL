3dc2e5474e7720a6b0360d931d21b400ab4e74d8
Nguyenquynhanh2001 <136559568+Nguyenquynhanh2001@users.noreply.github.com>
2023-12-21 13:37:59 +0700

    Update pratice4.sql

--- pratice4.sql
+++ pratice4.sql
@@ -16,7 +16,11 @@ WHEN x+y>z AND x+z>y AND y+z>x THEN 'Yes' ELSE 'No'
 END AS triangle
 FROM Triangle 
 --ex3
-
+SELECT
+CEILING(CAST(
+COUNT(CASE WHEN COALESCE(call_category) OR call_category='n/a' THEN 'j' END)
+*100/COUNT(call_category)) AS DECIMAL,1) AS call_percentage
+FROM callers;
 --ex4
 
 --ex5
