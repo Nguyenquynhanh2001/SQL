c11de860d8f96e7c842f9ace9123506b26de05c4
Nguyenquynhanh2001 <136559568+Nguyenquynhanh2001@users.noreply.github.com>
2023-12-24 14:43:45 +0700

    Update practice_5.sql

--- practice_5.sql
+++ practice_5.sql
@@ -45,3 +45,84 @@ SELECT pages.page_id  FROM pages
 LEFT JOIN page_likes ON pages.page_id = page_likes.page_id
 WHERE page_likes.user_id IS NULL
 ORDER BY pages.page_id ASC
+
+-- Mid-course test
+
+--ex1:
+SELECT DISTINCT replacement_cost FROM film
+
+SELECT MIN(replacement_cost) FROM film
+
+--ex2:
+SELECT 
+COUNT(CASE WHEN replacement_cost BETWEEN 9.99 AND 19.99 THEN 'low'
+END) AS low,
+COUNT(CASE WHEN replacement_cost BETWEEN 20.00 AND 24.99 THEN 'medium'
+END) AS medium, 
+COUNT(CASE WHEN replacement_cost BETWEEN 25.00 AND 29.99 THEN 'high'
+END )AS high 
+FROM film
+
+--ex3:
+SELECT title, film.length,category.name
+FROM film 
+LEFT JOIN film_category ON film.film_id = film_category.film_id
+JOIN category ON film_category.category_id = category.category_id 
+WHERE category.name IN ('Drama','Sports')
+ORDER BY film.length DESC 
+
+--ex4:
+SELECT category.name, COUNT(title)
+FROM film 
+LEFT JOIN film_category ON film.film_id = film_category.film_id
+JOIN category ON film_category.category_id = category.category_id 
+GROUP BY category.name
+ORDER BY COUNT(title) DESC
+
+--ex5:
+SELECT CONCAT(
+  actor.first_name
+  ,' '
+  ,actor.last_name
+  ) AS fullname, COUNT(*)
+FROM actor
+JOIN film_actor ON actor.actor_id = film_actor.actor_id
+GROUP BY fullname
+ORDER BY COUNT(*) DESC
+
+--ex6:
+SELECT COUNT(*)
+FROM address
+LEFT JOIN customer ON address.address_id = customer.address_id
+WHERE customer_id IS NULL 
+
+--ex7:
+SELECT city.city_id, city.city, SUM(amount)
+FROM city
+JOIN address ON city.city_id = address.city_id
+JOIN customer ON address.address_id= customer.address_id
+JOIN payment ON customer.customer_id = payment.customer_id
+GROUP BY city.city_id, city.city 
+ORDER BY SUM(amount) DESC
+
+--ex8:
+SELECT CONCAT(
+  city.city
+  ,','
+  ,country.country
+  ) AS city_country, SUM(amount)
+FROM country
+JOIN city ON country.country_id = city.country_id
+JOIN address ON city.city_id = address.city_id
+JOIN customer ON address.address_id= customer.address_id
+JOIN payment ON customer.customer_id = payment.customer_id
+GROUP BY country.country,city.city 
+ORDER BY SUM(amount) DESC
+
+
+
+
+
+
+
+
